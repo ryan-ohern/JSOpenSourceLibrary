e5ae633d00e6ec8fe59ac3a302a94f248b02c050
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 09:53:00 -0400

    strings

---
+++ js/1.10_strings.js
@@ -0,0 +1,13 @@
+//assigning strings to variables
+var myCatsName = "Jack Jack";
+var myCatsColor = "brown";
+var myCatsAge = 7;
+
+//concatonation
+var myCatInfo = myCatsName + ", " + myCatsColor + ", " + myCatsAge;
+console.log(myCatInfo);
+
+var lyric1 = "Happy Birthday";
+var lyric2 = " to you!";
+var lyricsToHappyBirthday = lyric1.concat(lyric2);
+console.log(lyricsToHappyBirthday);
