e32cbcdeda2bc1356ada2c5271bda4f8f5787074
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 10:46:11 -0400

    iterating over arrays

---
+++ js/1.61_arrays_loops.js
@@ -0,0 +1,10 @@
+//ARRAYS & LOOPS
+
+//create an array
+var citiesLived = ["Kansas City", "Nashville", "Indianapolis", "Boston"]
+
+//city is the index value
+for (city in citiesLived){
+	console.log(citiesLived[city]);
+}
+
