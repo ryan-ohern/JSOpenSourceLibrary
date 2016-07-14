42ac36c083f07e90227fd9feba44165dbfd57d6f
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 10:03:48 -0400

    conditionals

---
+++ js/1.30_conditionals.js
@@ -0,0 +1,23 @@
+//CONDITIONALS
+
+var x = 10;
+var y = 5;
+
+//comparing values
+if (x > y){
+	console.log("Yes, x is greater than y");
+} else {
+	console.log("Nope, x is not greater than y");
+}
+
+//conditionals with booleans
+var doYouLikeCookies = false;
+var willIgiveYouACookie = false;
+
+if (doYouLikeCookies === true){
+	console.log("Yeah you do!");
+	willIgiveYouACookie = true;
+} else {
+	console.log("What's wrong with you?");
+}
+
