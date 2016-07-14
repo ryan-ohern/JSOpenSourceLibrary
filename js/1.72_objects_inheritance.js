19ef47a7c0d7fd03cbadcff4d0e044ff90d2e513
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 11:29:44 -0400

    added method inside object method

--- js/1.72_objects_inheritance.js
+++ js/1.72_objects_inheritance.js
@@ -12,12 +12,20 @@ var house = {
 		return this.streetAddress + " " +
 		this.city + ", " +
 		this.state;
+	},
+	//method within an object method
+	getHouseInfo: function(){
+		return "This house is "+ this.age + " years old and is located at " + this.location();
 	}
 };
 
+//creating a new object with inheritance from "house"
 var myNewHouse = Object.create(house)
+//passing values
 myNewHouse.streetAddress = "123 Awesome Street Lane";
 myNewHouse.city = "Fishers";
 myNewHouse.state = "IN";
 myNewHouse.age = 10;
+
 console.log(myNewHouse.location());
+console.log(myNewHouse.getHouseInfo());
