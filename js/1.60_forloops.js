f0e68372fb3625d5225314c5a8b1ef8148f82f0d
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 10:28:16 -0400

    for loops

---
+++ js/1.60_forloops.js
@@ -0,0 +1,19 @@
+//FOR LOOPS
+
+
+for (var i=0; i <= 10; i++) {
+	console.log(i);
+};
+
+
+//LOOPS with CONDITIONALS
+var levelOfAnxiety = 0;
+
+for (var levelOfAnxiety=0; levelOfAnxiety < 11; levelOfAnxiety++) {
+	if (levelOfAnxiety < 10) {
+		console.log("I'm doing great");
+	} else {
+		console.log("Ahhh!!!");
+	}
+};
+
