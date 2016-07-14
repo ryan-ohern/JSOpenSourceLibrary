17bb6745f98c5664ffd4ff9690adbc09b97cb722
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 10:52:49 -0400

    object properties

---
+++ js/1.70_objects_properties.js
@@ -0,0 +1,13 @@
+//OBJECT PROPERTIES
+
+var house = {
+	streetAddress: 	"4049 Stonybrook Dr.",
+	city:			"Nashville",
+	state:			"TN",
+	age: 			9,
+	color: 			"faded yellow",
+	size: 			"1400 sq ft" 
+};
+
+
+console.log(house);
