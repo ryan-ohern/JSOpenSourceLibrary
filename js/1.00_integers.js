291fb1e861f1d0612d3dcd0348d166ab2c45e79f
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 09:45:37 -0400

    integers

--- js/1.00_integers.js
+++ js/1.00_integers.js
@@ -1,4 +1,33 @@
-var x = 2;
-var y = 3;
 
+var x = 10;
+var y = 2;
+
+//adding integers
 console.log(x + y);
+
+//subtracting integers
+console.log(x - y);
+
+//multiply integers
+console.log(x * y);
+
+//diving integers
+console.log(x / y);
+
+//remainders
+var a = 15;
+var b = 4;
+console.log(a % b);
+
+//modifying integers
+numberOfTimesYellingAtTerminalErrors = 10;
+
+//adding
+numberOfTimesYellingAtTerminalErrors += 2;
+console.log(numberOfTimesYellingAtTerminalErrors);
+
+//subtracting
+numberOfTimesYellingAtTerminalErrors -= 2;
+console.log(numberOfTimesYellingAtTerminalErrors);
+
+
