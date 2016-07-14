e6cf70d25bbae1237d6c36fbe4bca72c451091b9
Ryan O'Hern <ryanohern@gmail.com>
2016-07-14 10:11:24 -0400

    functions intro

---
+++ js/1.60_functions.js
@@ -0,0 +1,19 @@
+//FUNCTIONS INTRO
+
+//adding numbers
+var x = 10;
+var y = 5;
+
+function addNumbers(){
+	console.log(x + y);
+}
+
+//calling a function
+addNumbers();
+
+//passing values
+function addVariables(a, b){
+	console.log(a + b);
+}
+
+addVariables(65, 10);
