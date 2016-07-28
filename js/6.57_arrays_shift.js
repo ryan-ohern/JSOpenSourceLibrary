3607bfa2a6c185d230a90c7dafbbcced19e3e03d
Ryan O'Hern <ryanohern@gmail.com>
2016-07-28 10:14:44 -0400

    arrays - shift

---
+++ js/6.57_arrays_shift.js
@@ -0,0 +1,10 @@
+// ARRAYS - SHIFT
+
+var listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// shift removes first item from an array and returns the value 
+var shiftedNumber = listOfNumbers.shift();
+
+console.log(shiftedNumber);
+
+console.log(listOfNumbers);
