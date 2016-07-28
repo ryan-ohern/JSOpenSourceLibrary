01362aaac52db0f6fbdefeba3f2a0f2f129e9ada
Ryan O'Hern <ryanohern@gmail.com>
2016-07-28 10:00:31 -0400

    arrays - pop & push

---
+++ js/6.52_arrays_pop.js
@@ -0,0 +1,8 @@
+// ARRAYS - POP
+
+var listOfColors = ['blue', 'green', 'yellow', 'red'];
+
+// removes last item in array
+listOfColors.pop();
+
+console.log(listOfColors);
---
+++ js/6.53_arrays_push.js
@@ -0,0 +1,8 @@
+// ARRAYS - PUSH
+
+var listOfColors = ['blue', 'green', 'yellow', 'red'];
+
+// adds item to end of array
+listOfColors.push('purple');
+
+console.log(listOfColors);
