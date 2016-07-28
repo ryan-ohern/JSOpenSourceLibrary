61f7ddd82f88e4dbdf5422d8221dcb73914cee11
Ryan O'Hern <ryanohern@gmail.com>
2016-07-28 10:08:38 -0400

    slicing arrays

---
+++ js/6.55_arrays_slice.js
@@ -0,0 +1,10 @@
+// ARRAYS - SLICE
+
+// slice copies a portion of an array
+
+var listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// first parameter is starting index, second parameter is stopping index
+var copiedNumbers = listOfNumbers.slice(2, 5);
+
+console.log(copiedNumbers);
