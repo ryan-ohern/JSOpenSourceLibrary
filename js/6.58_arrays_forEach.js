9082ae4d445f80908a73f53469b8c8f5d38420e5
Ryan O'Hern <ryanohern@gmail.com>
2016-07-29 09:58:55 -0400

    arrays - for each

---
+++ js/6.58_arrays_forEach.js
@@ -0,0 +1,7 @@
+var arr = [1, 2, 3, 4];
+var sum = 0;
+function myFunction(itemValue, itemIndex) {
+	sum += itemValue;
+	console.log("itemIndex: " + itemIndex + " - Sum: " + sum);
+}
+arr.forEach(myFunction);
